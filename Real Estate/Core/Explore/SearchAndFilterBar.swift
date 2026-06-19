75e888256739e14d04ae3860d5ebd6b1043cb8fa
Nadine Agbortarh <Nadinetarh@gmail.com>
2026-06-19 12:12:53 +0200

    Add placeholder texts and button to search bar
    Populate SearchAndFilterBar with placeholder labels and a trailing action button. Adds a 'Where to?' caption and a secondary 'Anywhere -Any week -Add guests' caption2 styled gray, inserts a Spacer for layout, and a placeholder Button with an empty action.

--- Real Estate/Core/Explore/SearchAndFilterBar.swift
+++ Real Estate/Core/Explore/SearchAndFilterBar.swift
@@ -13,8 +13,20 @@ struct SearchAndFilterBar: View {
             Image(systemName: "magnifyingglass")
             
             VStack(aligment: .leading, spacing: 2) {
+                Text("Where to?")
+                    .font(.caption)
+                    .fontWeight(.semibold)
+                
+                Text("Anywhere -Any week -Add guests")
+                    .font(.caption2)
+                    .foregroundStyle(.gray)
                 
             }
+            Spacer()
+            
+            Button(action: {}, label: {
+                Text("Button")
+            })
             
         }
     }
